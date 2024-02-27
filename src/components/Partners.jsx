30cc8bb6c535027e10832f75f37bd6430c605f47
Kamil Markowski <kamil0markowski@gmail.com>
2024-02-27 22:29:51 +0100

    partners section pagination added

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -5,26 +5,59 @@ import partners from "../assets/data/partners.json";
 
 export default function Partners() {
 
-    // console.log(partners);
-
     const [partnerFilter, setPartnerFilter] = useState("foundation");
     const [partnersFiltered, setPartnersFiltered] = useState(partners.categories.find(category => category.name === "foundation").elements);
+    const numPages = Math.ceil(partnersFiltered.length / 3);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [partnersShowing, setPartnersShowing] = useState(partnersFiltered.slice(0, 3));
+
+    const renderPageNumbers = () => {
+        const pageNumbers = [<button key={1} className={currentPage === 1 ? "active" : null } onClick={() => handlePageChange(1)}>{1}</button>];
+        for (let i = 2; i <= numPages; i++) {
+            pageNumbers.push(
+                <button key={i} className={currentPage === i ? "active" : null} onClick={() => handlePageChange(i)}>{i}</button>
+            );
+        }
+        return pageNumbers;
+    };
+
+    const handlePageChange = (pageNumber) => {
+        const startIndex = (pageNumber - 1) * 3;
+        const endIndex = Math.min(startIndex + 3, partnersFiltered.length);
+        setPartnersShowing(partnersFiltered.slice(startIndex, endIndex));
+        setCurrentPage(pageNumber);
+    };
 
     useEffect(() => {
         setPartnersFiltered(partners.categories.find(category => category.name === partnerFilter).elements);
+        setPartnersShowing(partners.categories.find(category => category.name === partnerFilter).elements.slice(0, 3));
+        setCurrentPage(1);
     }, [partnerFilter]);
 
-    const foundationInfo = "In our database you will find a list of verified Foundations with which we cooperate. You can check what they do, who they help and what they need."
 
     return <section className="partners__container">
         <h2 className="partners__title">Whom do we support?</h2>
         <div className="decoration"></div>
         <div className="partner__filters">
-            <button className={partnerFilter === "foundation" ? "active" : null} onClick={() => setPartnerFilter("foundation")}>Foundations</button>
-            <button className={partnerFilter === "NGO" ? "active" : null} onClick={() => setPartnerFilter("NGO")}>Non Governmental Organisations</button>
-            <button className={partnerFilter === "Local Donations Collection" ? "active" : null} onClick={() => setPartnerFilter("Local Donations Collection")}>Local Collections</button>
+            <button
+                className={partnerFilter === "foundation" ? "active" : null}
+                onClick={() => setPartnerFilter("foundation")}
+            >Foundations</button>
+            <button
+                className={partnerFilter === "NGO" ? "active" : null}
+                onClick={
+                () => setPartnerFilter("NGO")}
+            >Non Governmental Organisations</button>
+            <button
+                className={partnerFilter === "Local Donations Collection" ? "active" : null}
+                onClick={
+                () => setPartnerFilter("Local Donations Collection")}
+            >Local Collections</button>
         </div>
         <p className="partners__note">In our database you will find a list of verified foundations, NGOs and Local Donations Collections with which we cooperate. You can check what they do, who they help and what they need.</p>
-        <PartnersList partnersFiltered={partnersFiltered}/>
+        <PartnersList partnersShowing={partnersShowing}/>
+        <div className="pagination">
+            {renderPageNumbers()}
+        </div>
     </section>
 }
--- src/components/PartnersList.jsx
+++ src/components/PartnersList.jsx
@@ -1,6 +1,6 @@
-export default function PartnerList({partnersFiltered}) {
+export default function PartnerList({partnersShowing}) {
     return <div className="partners__list">
-        {partnersFiltered.map((partner, index) =>
+        {partnersShowing.map((partner, index) =>
             <div key={index} className="partner__entry">
                 <div className="partner__about">
                     <div className="partner__name">{partner.name}</div>
